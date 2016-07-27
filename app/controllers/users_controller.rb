0522b44229bdce5b5be03ecbd2a6a6153109c377
Cess <cessmbuguar@gmail.com>
2016-07-27 16:35:42 +0300

    show and edit templates and their methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,23 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+  def show
+    @user = User.find(params[:id])
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(join_params)
+   if @user.save
+     redirect_to @user
+   else 
+     render 'edit'
+   end
+  end
+
+  private
   def join_params
     params.require(:user).permit( :name, :email, :password, :password_confirmation )
   end
