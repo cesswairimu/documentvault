b94061b16a0e818ff3671a93b9259067dd0adad6
Cess <cessmbuguar@gmail.com>
2016-07-28 10:37:27 +0300

    add login and logout actions when creating and destroying a session

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,4 +1,21 @@
 class LoginsController < ApplicationController
   def new
   end
+  def create 
+    user = User.find_by(name: params[:login][:name])
+    if user && user.authenticate(params[:login][:password])
+      flash[:yeah]= "Welcome back"
+      log_in user 
+      redirect_to user
+    else
+      flash[:nasty] = "Check your email and password Bunch"
+      render 'new'
+    end
+  end
+  
+
+  def destroy
+    logout
+    redirect_to root_url
+  end
 end
