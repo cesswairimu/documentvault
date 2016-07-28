30f2ad92485a2e7df2744607fc99bc711a0b00ea
Cess <cessmbuguar@gmail.com>
2016-07-28 10:35:28 +0300

    add login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'join' => 'users#new'
   get 'login' => 'logins#new'
   post 'login' => 'logins#create'
+  delete 'logout' => 'logins#destroy'
   resources :users
   resources :logins
+  
 end
