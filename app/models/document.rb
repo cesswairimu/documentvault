a524488a33b578bfee71b69da6d9468423baa49b
Cess <cessmbuguar@gmail.com>
2016-07-29 17:31:27 +0300

    remove scope

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,10 +1,9 @@
 class Document < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { maximum: 20 }
-  validates :link, presence: true, length: { maximum: 50 }
+  validates :link, presence: true, length: { maximum: 50 }, uniqueness:true
   validates :tag, presence: true
   validates :department, presence: true
   validates :content, presence: true, length: {minimum: 50 }
-  DEPARTMENT_LIST = [ "Operations", "Training", "Marketing","Computing", "Accounting", ]
-  scope :a, -> { where("department") }
+  DEPARTMENT_LIST = [ "Operations", "Training", "Marketing","Computing", "Accounting" ]
 end
