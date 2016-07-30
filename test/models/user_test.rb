10934c8ad917f466c3a249f59c8ab1a35adf3e49
Cess <cessmbuguar@gmail.com>
2016-07-30 13:03:38 +0300

    test email, name, and password validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Cess", email: "cess@gmail.com", password: "terrence", password_confirmation: "terrence")
+  end
+
+ test "name should be present" do
+   @user.name = " "
+     assert_not @user.valid?
+   end
+ test "email should be present" do
+   @user.email =""
+     assert_not @user.valid?
+ end
+
+ test "name should have maximum length" do
+   @user.name = "a" * 21
+   assert_not @user.valid?
+ end
+
+ test "email should not be more than 30 xters" do
+   @user.email = "c" * 31
+   assert_not @user.valid?
+ end
+
+ test "password should have minimum length" do
+   @user.password = @user.password_confirmation = "a" * 3
+   assert_not @user.valid?
+ end
+ test "email validation should accept valid addresses" do
+   valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+   first.last@foo.jp alice+bob@baz.cn]
+   valid_addresses.each do |valid_address|
+     @user.email = valid_address
+     assert @user.valid?, "#{valid_address.inspect} should be valid"
+   end
+ end
 end
