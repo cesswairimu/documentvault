a1514b365ee5f777369c5731b1c8e76783ea755f
Cess <cessmbuguar@gmail.com>
2016-07-30 11:18:09 +0300

    add a self.search method to search according to the title

--- app/models/document.rb
+++ app/models/document.rb
@@ -6,4 +6,8 @@ class Document < ApplicationRecord
   validates :department, presence: true
   validates :content, presence: true, length: {minimum: 50 }
   DEPARTMENT_LIST = [ "Operations", "Training", "Marketing","Computing", "Accounting" ]
+
+  def self.search(search)
+    where("title like?",  "%#{search}%")
+  end
 end
