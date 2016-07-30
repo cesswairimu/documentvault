6d39fae6e8f1b175dfb2a619f26ec7c8e910aeee
Cess <cessmbuguar@gmail.com>
2016-07-30 13:05:31 +0300

    removed the action new test

--- test/controllers/documents_controller_test.rb
+++ test/controllers/documents_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class DocumentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get documents_new_url
-    assert_response :success
-  end
-
+  
 end
