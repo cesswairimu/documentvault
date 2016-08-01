27de68b6f28db97951ddc58a6490d192a753cc3a
Cess <cessmbuguar@gmail.com>
2016-08-01 23:10:54 +0300

    create migration for adding column account activation to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729072416) do
+ActiveRecord::Schema.define(version: 20160801133953) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "title"
@@ -28,10 +28,13 @@ ActiveRecord::Schema.define(version: 20160729072416) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
   end
 
 end
