c0bf27e387c87ea750ee7c479f7ad75d6fd82b66
Cess <cessmbuguar@gmail.com>
2016-08-02 15:36:04 +0300

    arranged the users in a table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,6 @@ class UsersController < ApplicationController
 
   def correct_user
     @user = User.find(params[:id])
-    redirect_to (root_url) unless @user == current_user?(@user)
+    redirect_to (root_url) unless @user == current_user
   end
 end
