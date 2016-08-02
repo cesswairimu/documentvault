999bc2a1a2389aaaaa99c2846c3a0b6a0d703525
Cess <cessmbuguar@gmail.com>
2016-08-02 15:28:23 +0300

    add name to be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   before_save :email_downcase
   before_create :activation_digest
   REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true,
     format: { with: REGEX }, length: { maximum: 30 } 
   validates :password, length: { minimum: 5  }
