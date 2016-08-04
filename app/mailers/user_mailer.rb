e1951b15856be47e8ed0200a4c66dda5eed58d28
Cess <cessmbuguar@gmail.com>
2016-08-04 17:46:53 +0300

    mailer methods password and account activations

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account Acctivation"
+    mail to: user.email, subject: "Account Activation"
   end
 
   def password_reset(user)
