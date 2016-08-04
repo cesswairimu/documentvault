9e67ad9d864b954f1de1878eb21c792cdd489144
Cess <cessmbuguar@gmail.com>
2016-08-04 17:44:08 +0300

    change name for email downcasing and activation_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :documents
   attr_accessor :remember_token, :activation_token
-  before_save :email_downcase
-  before_create :activation_digest
+  before_save :downcase_email
+  before_create :create_activation_digest
   REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true,
@@ -41,10 +41,10 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
   private
-  def email_downcase
+  def downcase_email
     self.email = email.downcase
   end
-  def activation_digest
+  def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest= User.digest(activation_token)
   end
