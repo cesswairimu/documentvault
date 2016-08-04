b9525cc2bab37f6d9162882c5a43f49dfcf84f4c
Cess <cessmbuguar@gmail.com>
2016-08-04 17:32:21 +0300

    define method logged_in_user to be available to all the controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include LoginsHelper
+  private
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:nasty] = "Login in first Bunch!!"
+      redirect_to login_path
+    end
+  end
+
 end
