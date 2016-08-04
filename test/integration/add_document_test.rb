922bda433e15640e223adf770a5a35eb1b63c440
Cess <cessmbuguar@gmail.com>
2016-08-04 17:35:43 +0300

    test successful and unsuccessful adddition of a document

--- test/integration/add_document_test.rb
+++ test/integration/add_document_test.rb
@@ -1,22 +1,43 @@
 require 'test_helper'
 
 class AddDocumentTest < ActionDispatch::IntegrationTest
-def setup
-  @current_user = users(:cess)
-end
-
-   test "the adding a new document" do
-     assert_not  @current_user.nil?
-     get new_document_path
-     post documents_path params: {
-       document: {
-         title: "fgfhsgsjjs", link: "hdshdgfjdh", tag: "ghdhgdg", 
-         department: "Training", content: "the earth is round you know i know that and i dont believe it why dont i see the
-         moon in my room, crazy right? like why would it be there"
-
-       }
-     }
-       assert_template 'documents/show'
-       assert_not flash.empty?
+  def setup
+    @user = users(:cess)
   end
+
+  test "Unsuccessful document addition" do
+    log_in_as(@user, remember_me: '1')
+    get new_document_path
+    assert_template 'documents/new'
+    assert_no_difference 'Document.count' do
+      post documents_path params: {
+        document: { title: "", tag: "", link:"",
+                    department: "", content: ""
+
+      }
+      }
+    end
+    assert_not flash.empty?
+    assert_template 'documents/new'
+    # assert_select "ul", "have"
+  end
+
+  test "the adding a new document" do
+    log_in_as(@user, remember_me: '1')
+    get new_document_path
+    assert_template 'documents/new'
+   assert_difference 'Document.count', 1 do
+    post documents_path params: {
+      document: {
+        title: "fgfhsgsjjs", link: "hdshdgfjdh", tag: "ghdhgdg", 
+        department: "Training", content: "the earth is round you know i know that and i dont believe it why dont i see the
+        moon in my room, crazy right? like why would it be there"
+
+      }
+    }
+  end
+  follow_redirect!
+  assert_template 'documents/show'
+  assert_not flash.empty?
+end
 end
