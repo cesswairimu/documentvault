529987f48106d1fa836a706cb1d8844f0e5c676e
Cess <cessmbuguar@gmail.com>
2016-08-05 16:35:45 +0300

    add column password_reset to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801133953) do
+ActiveRecord::Schema.define(version: 20160805103506) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20160801133953) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
